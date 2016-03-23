3572eb56ad0034d908960265e24382ff5f5fa9c0
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-03-23 10:56:56 +0300

    SqlDefaultValueAttribute

---
+++ Solution/Rib.Ef/Metadata/SqlDefaultValueAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Rib.Ef.Metadata
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class SqlDefaultValueAttribute : Attribute
+    {
+        public SqlDefaultValueAttribute(string sqlDefaultValue)
+        {
+            SqlDefaultValue = sqlDefaultValue;
+        }
+
+        public string SqlDefaultValue { get; }
+    }
+}
