5bd1552414e41061518bcf9f83c6f293bf5d3098
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-03-23 10:54:39 +0300

    Function convention

--- Solution/Rib.Ef/Conventions/FunctionConvention.cs
+++ Solution/Rib.Ef/Conventions/FunctionConvention.cs
@@ -1,27 +1,84 @@
 ﻿namespace Rib.Ef.Conventions
 {
     using System;
+    using System.Data.Entity;
     using System.Data.Entity.Core.Metadata.Edm;
     using System.Data.Entity.Infrastructure;
     using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+    using System.Reflection;
+    using JetBrains.Annotations;
+    using Rib.Ef.Helpers;
 
     public class FunctionConvention : IStoreModelConvention<EdmModel>
     {
-        private readonly Type _functionsDefineType;
+        [NotNull] private readonly Type _functionsDefineType;
 
-        public FunctionConvention(Type functionsDefineType)
+        public FunctionConvention([NotNull] Type functionsDefineType)
         {
             if (functionsDefineType == null) throw new ArgumentNullException(nameof(functionsDefineType));
             _functionsDefineType = functionsDefineType;
         }
 
         /// <summary>
-        /// Applies this convention to an item in the model.
+        ///     Applies this convention to an item in the model.
         /// </summary>
-        /// <param name="item">The item to apply the convention to.</param><param name="model">The model.</param>
-        public void Apply(EdmModel item, DbModel model)
+        /// <param name="item">The item to apply the convention to.</param>
+        /// <param name="model">The model.</param>
+        public void Apply([NotNull] EdmModel item, [NotNull] DbModel model)
         {
-            throw new System.NotImplementedException();
+            var functions = _functionsDefineType
+                    .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Select(x => new
+                    {
+                        Attr = x.GetCustomAttribute<DbFunctionAttribute>(),
+                        Method = x
+                    })
+                    .Where(x => x.Attr != null)
+                    .Select(x => new
+                    {
+                        x.Attr,
+                        x.Method,
+                        Parameters = x.Method.GetParameters()
+                                      .Select(Parameter.Create)
+                                      .Select(p => p.ToFunctionParameter(model, ParameterMode.In)),
+                        ReturnValue = Parameter.Create(x.Method.ReturnParameter).ToFunctionParameter(model, ParameterMode.ReturnValue)
+                    })
+                    .Select(f => item.CreateFunction(f.Attr.FunctionName, f.Parameters.ToList(), new[] {f.ReturnValue}));
+            foreach (var function in functions)
+            {
+                item.AddItem(function);
+            }
+        }
+
+        private struct Parameter
+        {
+            private Parameter(PrimitiveTypeKind kind, [NotNull] string name)
+            {
+                Kind = kind;
+                Name = name;
+            }
+
+            public static Parameter Create([NotNull] ParameterInfo info)
+            {
+                if (info == null) throw new ArgumentNullException(nameof(info));
+                PrimitiveTypeKind kind;
+                if (!PrimitiveTypeKindResolver.TryGetPrimitiveTypeKind(info.ParameterType, out kind))
+                {
+                    throw new InvalidOperationException($"{info.ParameterType} is undefined PrimitiveTypeKind");
+                }
+                return new Parameter(kind, info.Name);
+            }
+
+            public FunctionParameter ToFunctionParameter([NotNull] DbModel model, ParameterMode mode)
+            {
+                return FunctionParameter.Create(Name, model.GetStorePrimitiveType(Kind), mode);
+            }
+
+            private PrimitiveTypeKind Kind { get; }
+
+            [NotNull]
+            private string Name { get; }
         }
     }
 }
---
+++ Solution/Rib.Ef/Helpers/PrimitiveTypeKindResolver.cs
@@ -0,0 +1,108 @@
+﻿namespace Rib.Ef.Helpers
+{
+    using System;
+    using System.Data.Entity.Core.Metadata.Edm;
+    using System.Data.Entity.Spatial;
+    using JetBrains.Annotations;
+
+    public static class PrimitiveTypeKindResolver
+    {
+        /// <summary>
+        ///     Returns the <see cref="PrimitiveTypeKind" /> corresponding to the given CLR type
+        /// </summary>
+        /// <param name="clrType"> The CLR type for which the PrimitiveTypeKind value should be resolved </param>
+        /// <param name="resolvedPrimitiveTypeKind"> The PrimitiveTypeKind value to which the CLR type resolves, if any. </param>
+        /// <returns> True if the CLR type represents a primitive (EDM) type; otherwise false. </returns>
+        public static bool TryGetPrimitiveTypeKind([NotNull] Type clrType, out PrimitiveTypeKind resolvedPrimitiveTypeKind)
+        {
+            if (clrType == null) throw new ArgumentNullException(nameof(clrType));
+            PrimitiveTypeKind? primitiveTypeKind = null;
+            if (!clrType.IsEnum) // Enums return the TypeCode of their underlying type
+            {
+                // As an optimization, short-circuit when the provided type has a known type code.
+                switch (Type.GetTypeCode(clrType))
+                {
+                    // PrimitiveTypeKind.Binary = byte[] = TypeCode.Object
+                    case TypeCode.Boolean:
+                        primitiveTypeKind = PrimitiveTypeKind.Boolean;
+                        break;
+                    case TypeCode.Byte:
+                        primitiveTypeKind = PrimitiveTypeKind.Byte;
+                        break;
+                    case TypeCode.DateTime:
+                        primitiveTypeKind = PrimitiveTypeKind.DateTime;
+                        break;
+                    // PrimitiveTypeKind.DateTimeOffset = System.DateTimeOffset = TypeCode.Object
+                    case TypeCode.Decimal:
+                        primitiveTypeKind = PrimitiveTypeKind.Decimal;
+                        break;
+                    case TypeCode.Double:
+                        primitiveTypeKind = PrimitiveTypeKind.Double;
+                        break;
+                    // PrimitiveTypeKind.Geography = System.Data.Entity.Spatial.DbGeometry (or subtype) = TypeCode.Object
+                    // PrimitiveTypeKind.Geometry = System.Data.Entity.Spatial.DbGeometry (or subtype) = TypeCode.Object
+                    // PrimitiveTypeKind.Guid = System.Guid = TypeCode.Object
+                    case TypeCode.Int16:
+                        primitiveTypeKind = PrimitiveTypeKind.Int16;
+                        break;
+                    case TypeCode.Int32:
+                        primitiveTypeKind = PrimitiveTypeKind.Int32;
+                        break;
+                    case TypeCode.Int64:
+                        primitiveTypeKind = PrimitiveTypeKind.Int64;
+                        break;
+                    case TypeCode.SByte:
+                        primitiveTypeKind = PrimitiveTypeKind.SByte;
+                        break;
+                    case TypeCode.Single:
+                        primitiveTypeKind = PrimitiveTypeKind.Single;
+                        break;
+                    case TypeCode.String:
+                        primitiveTypeKind = PrimitiveTypeKind.String;
+                        break;
+                    // PrimitiveTypeKind.Time = System.TimeSpan = TypeCode.Object
+                    case TypeCode.Object:
+                        {
+                            if (typeof(byte[]) == clrType)
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.Binary;
+                            }
+                            else if (typeof(DateTimeOffset) == clrType)
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.DateTimeOffset;
+                            }
+                            // DbGeography/Geometry are abstract so subtypes must be allowed
+                            else if (typeof(DbGeography).IsAssignableFrom(clrType))
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.Geography;
+                            }
+                            else if (typeof(DbGeometry).IsAssignableFrom(clrType))
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.Geometry;
+                            }
+                            else if (typeof(Guid) == clrType)
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.Guid;
+                            }
+                            else if (typeof(TimeSpan) == clrType)
+                            {
+                                primitiveTypeKind = PrimitiveTypeKind.Time;
+                            }
+                            break;
+                        }
+                }
+            }
+
+            if (primitiveTypeKind.HasValue)
+            {
+                resolvedPrimitiveTypeKind = primitiveTypeKind.Value;
+                return true;
+            }
+            else
+            {
+                resolvedPrimitiveTypeKind = default(PrimitiveTypeKind);
+                return false;
+            }
+        }
+    }
+}
