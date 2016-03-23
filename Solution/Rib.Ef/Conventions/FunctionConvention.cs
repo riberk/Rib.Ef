e6b7d68632e3444ce6725b7600367cf209f99b46
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-03-23 10:00:18 +0300

    Function convention wip

---
+++ Solution/Rib.Ef/Conventions/FunctionConvention.cs
@@ -0,0 +1,27 @@
+﻿namespace Rib.Ef.Conventions
+{
+    using System;
+    using System.Data.Entity.Core.Metadata.Edm;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public class FunctionConvention : IStoreModelConvention<EdmModel>
+    {
+        private readonly Type _functionsDefineType;
+
+        public FunctionConvention(Type functionsDefineType)
+        {
+            if (functionsDefineType == null) throw new ArgumentNullException(nameof(functionsDefineType));
+            _functionsDefineType = functionsDefineType;
+        }
+
+        /// <summary>
+        /// Applies this convention to an item in the model.
+        /// </summary>
+        /// <param name="item">The item to apply the convention to.</param><param name="model">The model.</param>
+        public void Apply(EdmModel item, DbModel model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
