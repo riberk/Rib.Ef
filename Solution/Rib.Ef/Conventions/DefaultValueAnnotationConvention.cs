592614a1deeb99d90011708706ead40b1a05a9cc
p.kurdikov <p.kurdikov@ts-soft.ru>
2016-03-23 11:07:22 +0300

    Sql default value and default value convention

---
+++ Solution/Rib.Ef/Conventions/DefaultValueAnnotationConvention.cs
@@ -0,0 +1,15 @@
+﻿namespace Rib.Ef.Conventions
+{
+    using System.ComponentModel;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+
+    public class DefaultValueAnnotationConvention : AttributeToColumnAnnotationConvention<DefaultValueAttribute, object>
+    {
+        internal const string AnnotationName = "DefaultValue";
+
+        public DefaultValueAnnotationConvention() : base(AnnotationName, (propertyInfo, attributes) => attributes.Single().Value)
+        {
+        }
+    }
+}
---
+++ Solution/Rib.Ef/Conventions/SqlDefaultValueAnnotationConvention.cs
@@ -0,0 +1,15 @@
+﻿namespace Rib.Ef.Conventions
+{
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using System.Linq;
+    using Rib.Ef.Metadata;
+
+    public class SqlDefaultValueAnnotationConvention : AttributeToColumnAnnotationConvention<SqlDefaultValueAttribute, string>
+    {
+        internal const string AnnotationName = "SqlDefaultValue";
+
+        public SqlDefaultValueAnnotationConvention() : base(AnnotationName, (propertyInfo, attributes) => attributes.Single().SqlDefaultValue)
+        {
+        }
+    }
+}
